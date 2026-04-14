88fd004e725f714ce4668cc8c094538c1f667765
rbost656 <465418785@qq.com>
2026-04-14 05:07:55 -0700

    feat: include RequestContext in handler traits

--- crates/todo/src/application/handler.rs
+++ crates/todo/src/application/handler.rs
@@ -1,17 +1,19 @@
 use async_trait::async_trait;
 
 use crate::DomainError;
+use super::context::RequestContext;
+
 
 #[async_trait]
 pub trait CommandHandler<C>: Send + Sync {
     type Output: Send;
 
-    async fn handle(&self, cmd: C) -> Result<Self::Output, DomainError>;
+    async fn handle(&self, ctx: &RequestContext, Ccmd: C C) -> Result<Self::Output, DomainError>;
 }
 
 #[async_trait]
 pub trait QueryHandler<Q>: Send + Sync {
     type Output: Send;
 
-    async fn handle(&self, query: Q) -> Result<Self::Output, DomainError>;
+    async fn handle(&self, ctx: &RequestContext, query: Q) -> Result<Self::Output, DomainError>;
 }
